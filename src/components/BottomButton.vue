4c3fa2ccbd4862e00b207efb6da4f2d8ac04f798
cccatmint-Y <cao9710@outlook.com>
2022-04-15 00:25:36 +0800

    ADD component - BottomButton

---
+++ src/components/BottomButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="bottom-button-container" :style="`color:${color[0]};background-Color:${color[1]};`" @click="handleClick">
+        <i :class="['iconfont', icon ]"></i>
+        <slot>按钮</slot>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String
+    },
+    color: {
+      type: Array
+    }
+  },
+  setup () {
+    function handleClick () {
+      console.log('click')
+    }
+    return {
+      handleClick
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.bottom-button-container {
+    cursor: pointer;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,10 @@
       <div class="col-3 bg-danger left-panel">
         <FileSearch title="我的云文档" @search="handleSearch" />
         <FileList :files="defaultFiles || []"/>
+        <div class="row d-flex justify-content-between align-items-center">
+          <BottomButton icon="icon-add" :color="['white', 'green']" class="col-6">新建</BottomButton>
+          <BottomButton icon="icon-import" :color="['white', 'blue']" class="col-6">导入</BottomButton>
+        </div>
       </div>
       <div class="col-9 bg-primary right-panel">right</div>
     </div>
@@ -13,9 +17,10 @@
 <script>
 import FileSearch from '../components/FileSearch.vue'
 import FileList from '../components/FileList.vue'
+import BottomButton from '../components/BottomButton.vue'
 import { defaultFiles } from '../components/filesData.js'
 export default {
-  components: { FileSearch, FileList },
+  components: { FileSearch, FileList, BottomButton },
   setup () {
     function handleSearch (payload) {
       // todo: search handle
